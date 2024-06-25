c2c76bc5d5d5b993c0e176f2b887268ff93054b9
Lautaro Ruiz <lautaroruiz04@gmail.com>
2024-06-25 19:16:36 -0300

    Basic Components created: Navbar, main information, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar/navbar.component';
+import { PersonalInformationComponent } from './components/personal-information/personal-information/personal-information.component';
+import { HomeComponent } from './components/home/home/home.component';
+import { FooterComponent } from './components/footer/footer/footer.component';
+import { WorkExperienceComponent } from './components/work-experience/work-experience/work-experience.component';
+import { ProjectComponent } from './components/projects/project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PersonalInformationComponent,
+    HomeComponent,
+    FooterComponent,
+    WorkExperienceComponent,
+    ProjectComponent,
   ],
   imports: [
     BrowserModule,
