8ce47d420f5cf859b3ae09a07895222cc02c4796
Lautaro Ruiz <lautaroruiz04@gmail.com>
2024-07-05 20:22:26 -0300

    New styles added

--- src/app/components/about/about/about.component.ts
+++ src/app/components/about/about/about.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AboutComponent {
   education = [
     {title: 'Tec. Universitario en Programación', place: 'Universidad Tecnológica',  since: 'Mar. 2022', until: 'Dic. 2024', image: 'UTN_logo.jpg', width: 120,  height:95},
-    {title: 'Bachiller en Humanidades', place: 'Instituto San Antonio María Gianelli', since: 'Jun. 2016', until: 'Dic. 2021', image: 'UTN_logo.jpg', width: 120,  height:95}
+    {title: 'Bachiller en Humanidades', place: 'Instituto San Antonio María Gianelli', since: 'Jun. 2016', until: 'Dic. 2021', image: 'school.png', width: 110,  height:105}
   ]
 
   getImagePath(image: string): string {
--- src/app/components/work-experience/work-experience/work-experience.component.ts
+++ src/app/components/work-experience/work-experience/work-experience.component.ts
@@ -9,9 +9,9 @@ export class WorkExperienceComponent {
   work_experiences = [
     {
       charge: 'Back-end Developer', company: 'No Country', since: 'May. 2024', until: 'Jun. 2024', current_job: false, 
-      description: 'Simulación Tech donde trabajé como Backend Developer y los aspectos positivos de la misma son: ',
+      description: 'Simulación Laboral Tech donde trabajé como Backend Developer y los aspectos positivos que puedo destacar son: ',
       image: 'No-Country.webp', height: 95, width: 150 , mode: 'Remote',
-      positive_aspects: ["🔹 Apliqué Spring Security y aprendí a usar JWT", "🔹 Realicé tareas relacionadas a bases de datos y Java/Spring Boot.",  "🔹 Mejoré la comunicación en equipo al trabajar con mas roles que no sean solo backend.", "🔹 Trabajé con Sprints y con un TL.", "🔹 Daily Meetings."]
+      positive_aspects: ["Apliqué Spring Security y aprendí a usar JWT", "Realicé tareas relacionadas a bases de datos y Java/Spring Boot.",  "Mejoré la comunicación en equipo al trabajar en conjunto con varios roles.", "Trabajé con Sprints y con un TL.", "Daily Meetings."]
     }
   ]
 
