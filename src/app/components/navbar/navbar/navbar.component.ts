b1e8bbed0dae2263135ce107718e9067836c5319
Lautaro Ruiz <lautaroruiz04@gmail.com>
2024-07-25 12:36:14 -0300

    Testing navbar style

--- src/app/components/navbar/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar/navbar.component.ts
@@ -3,8 +3,7 @@ import { Component,ViewEncapsulation  } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
   isMenuOpen: boolean = false;
