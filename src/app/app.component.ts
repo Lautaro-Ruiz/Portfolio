4a0c7fbb7138af75f21afe77f8ac053ff524d4c0
Lautaro Ruiz <lautaroruiz04@gmail.com>
2024-12-18 17:51:16 -0300

    style: realice estilo de footer. Comence con el estilo del apartado de introduccion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { IntroductionSectionComponent } from './components/introduction-section/introduction-section.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ExperienceComponent } from './components/experience/experience/experience.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, FooterComponent, IntroductionSectionComponent, ProjectsComponent, ExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
