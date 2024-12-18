f53123f9278922e256c10358e0e8da967a284ccb
Lautaro Ruiz <lautaroruiz04@gmail.com>
2024-12-18 17:22:28 -0300

    feat: creación de componentes básicos para el portfolio

---
+++ src/app/components/introduction-section/introduction-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-introduction-section',
+  imports: [],
+  templateUrl: './introduction-section.component.html',
+  styleUrl: './introduction-section.component.css'
+})
+export class IntroductionSectionComponent {
+
+}
