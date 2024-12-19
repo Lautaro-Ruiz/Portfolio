e5f2261c8aa7b1d01ed3f454e8f930702053801a
Lautaro Ruiz <lautaroruiz04@gmail.com>
2024-12-19 12:32:40 -0300

    style: creacion de estructura html de proyectos y experiencia

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+  projects = [
+    {
+      name: 'MovieBox',
+      description: 'Description 1',
+      linkGithub: 'https://github.com/Lautaro-Ruiz/Movie-Box',
+      projectImages: [''],
+      technologiesImages: ['']
+    },
+    {
+      name: 'Salud Online',
+      description: 'Description 2',
+      linkGithub: 'https://github.com/bauticalvo/c-18-30-java-react',
+      projectImages: [''],
+      technologiesImages: ['']
+    }
+  ]
 
+  highlightWord(text: string, word: string): string {
+    const regex = new RegExp(`(${word})`, 'gi'); // Busca todas las apariciones, ignorando mayúsculas/minúsculas
+    return text.replace(regex, '<span class="highlight">$1</span>');
+  }
 }
