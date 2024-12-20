bce8e8e56937c0f12dd7464ef1752f2d9babb3b6
Lautaro Ruiz <lautaroruiz04@gmail.com>
2024-12-20 14:27:30 -0300

    style: definiendo el color de fondo de navbar y footer y otro para el menu principal

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -11,22 +11,41 @@ export class ProjectsComponent {
   projects = [
     {
       name: 'MovieBox',
-      description: 'Description 1',
+      description: 'Sistema de alquiler de películas. El usuario puede registrarse e iniciar sesión, ver películas, generar reseñas de las mismas, gestionar su lista de favoritos y carrito, y poder simular un proceso de compra de películas. El administrador es capaz de ver las distintas películas y opiniones de los usuarios y una lista de los mismos. Puede ver sus bibliotecas para poder devolver las películas cuando se acerquen al local, administrar pagos en efectivo, dar de baja cuentas registradas y finalmente gestionar las deudas de los usuarios.',
       linkGithub: 'https://github.com/Lautaro-Ruiz/Movie-Box',
       projectImages: ['Main menu.png', 'perfil.png', 'Proceso de compra.png', 'carrito.png', 'lista de usuarios.png'],
-      technologiesImages: ['AngularLogo.webp', 'typescript.svg', 'html5-fill.svg', 'css3-fill.svg']
+      technologiesImages: ['AngularLogo.webp', 'typescript.svg', 'html5-fill.svg', 'css3-fill.svg'],
+      currentImageIndex: 0 
     },
     {
       name: 'Salud Online',
-      description: 'Description 2',
+      description: 'Sistema que conecta a pacientes con una diversidad de especialistas. El usuario puede registrarse e iniciar sesion y ver los distintos medicos disponibles y a partir de allí, solicitar un turno. Por su parte, el médico, puede crear su perfil, detallar información sobre sus consultas médicas y recibir turnos.',
       linkGithub: 'https://github.com/bauticalvo/c-18-30-java-react',
       projectImages: ['Landing 1.png', 'Landing 2.png', 'Landing 3.png', 'Login.png', 'Perfil paciente.png'],
-      technologiesImages: ['reactjs-line.svg', 'tailwind-css-fill.svg','spring-logo.png', 'java-line.svg', 'mysql logo.png']
+      technologiesImages: ['reactjs-line.svg', 'tailwind-css-fill.svg','spring-logo.png', 'java-line.svg', 'mysql logo.png'],
+      currentImageIndex: 0 
     }
   ]
 
   highlightWord(text: string, word: string): string {
     const regex = new RegExp(`(${word})`, 'gi'); // Busca todas las apariciones, ignorando mayúsculas/minúsculas
     return text.replace(regex, '<span class="highlight">$1</span>');
+  }
+
+  previousImage(project: any): void {
+    if (project.currentImageIndex > 0) {
+      project.currentImageIndex--;
+    } else {
+      project.currentImageIndex = project.projectImages.length - 1; // Ir al final
+    }
+  }
+
+  // Cambiar a la siguiente imagen
+  nextImage(project: any): void {
+    if (project.currentImageIndex < project.projectImages.length - 1) {
+      project.currentImageIndex++;
+    } else {
+      project.currentImageIndex = 0; // Volver al inicio
+    }
   }
 }
