db569458a0305f3221e768396a2c1a7cdd9f5b81
Lautaro Ruiz <lautaroruiz04@gmail.com>
2024-12-21 18:10:35 -0300

    style: nuevo estilo para los proyectos y la experiencia. Nueva font family

--- src/app/components/experience/experience/experience.component.ts
+++ src/app/components/experience/experience/experience.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-experience',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
 export class ExperienceComponent {
-
+  description = [
+    'Desarrollo de sistema para conectar médicos con pacientes',
+    'Realización de tareas con Spring Boot y Spring Security',
+    'Uso de JWT',
+    'Gestión de base de datos relacional en MySQL',
+    'Comunicación efectiva en equipo entre varios roles y un Team Leader'
+  ]
 }
